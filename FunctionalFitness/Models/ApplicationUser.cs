9dab0ec996d7658e3f162d31fc0be53625b8a199
Richard Munie <richard.munie@gmail.com>
2018-10-06 14:06:51 -0500

    Init

---
+++ FunctionalFitness/Models/ApplicationUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace FunctionalFitness.Models
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool Admin { get; set; }
+        public DateTime RegDate { get; set; }
+        public int CoachID { get; set; }
+        public int Weight { get; set; }
+        public int Height { get; set; }
+    }
+}
